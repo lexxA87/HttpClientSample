9bd2eb7e0fd490c9004383c2e348fcd16004f10c
lexxA87 <alex.o.nikonov@gmail.com>
2023-03-03 21:21:30 +0400

    Added Card and Direction classes

---
+++ HttpClientSample/Card.cs
@@ -0,0 +1,15 @@
+﻿
+namespace HttpClientSample
+{
+    public class Card
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        // for converter in APIDbContext
+        // public int[]? GoToNumber { get; set; }
+
+        public List<Direction> Directions { get; set; } = new();
+    }
+}
---
+++ HttpClientSample/Direction.cs
@@ -0,0 +1,13 @@
+﻿
+namespace HttpClientSample
+{
+    public class Direction
+    {
+        public Guid Id { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public int NumberCard { get; set; }
+        public int CardId { get; set; }
+    }
+}
