5b137b3a71ecbf1cf99a2cc1a4d5c2775df83b0c
lexxA87 <alex.o.nikonov@gmail.com>
2023-03-03 21:57:29 +0400

    Added HttpClient and program

--- HttpClientSample/Program.cs
+++ HttpClientSample/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HttpClientSample;
+using System.Net.Http.Headers;
+
+HttpClient client = new();
+
+void ShowCard(Card card)
+{
+    Console.WriteLine($"Description: \t{card.Description}");
+}
+
+async Task<Card> GetCardAsync(string id)
+{
+    Card card = null;
+
+    HttpResponseMessage response = await client.GetAsync(id);
+    if (response.IsSuccessStatusCode)
+    {
+        card = await response.Content.ReadAsAsync<Card>();
+    }
+    return card;
+}
+
+async Task RunAsync()
+{
+    client.BaseAddress = new Uri("https://deathtrapdungeonapiver1.azurewebsites.net/api/card/");
+    client.DefaultRequestHeaders.Accept.Clear();
+    client.DefaultRequestHeaders.Accept.Add(
+        new MediaTypeWithQualityHeaderValue("application/json"));
+
+    try
+    {
+        Card card = new();
+        card = await GetCardAsync("35");
+        ShowCard(card);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+    Console.ReadLine();
+}
+
+RunAsync().GetAwaiter().GetResult();
